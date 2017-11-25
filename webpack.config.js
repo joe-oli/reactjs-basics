5adc697736bd8361efbb234c513a03d75b0b1c8e
joe-oli <joeSydneyAu@gmail.com>
2017-11-25 17:46:32 +1100

    initial commit, setup finished

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+var webpack = require("webpack");
+var path = require("path"); //nodejs lib
+
+var DIST_DIR = path.resolve(__dirname, "dist"); //dist folder will be created
+var SRC_DIR = path.resolve(__dirname, "src"); //src code
+
+var config = {
+    entry:  SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [{
+            test: /\.js?/,
+            include: SRC_DIR,
+            loader: "babel-loader",
+            query: {
+                presets: ["react", "es2015", "stage-2"]
+            }
+        }]
+    }
+};
+
+module.exports = config;
